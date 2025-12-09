cb83d4880e15c5695aa5ed33577863b9e4e31f9c
Wousato <arturgabanov435@gmail.com>
2025-12-09 11:49:03 +0300

    New commit

--- main.go
+++ main.go
@@ -2,21 +2,12 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"time"
 )
 
 func main() {
-	main1()
+	now := time.Now()
+	fmt.Print(now)
+	fmt.Println("Start")
 }
 
-
-
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
-}
-
-func main1() {
-	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
-}
